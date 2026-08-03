d0ef585ca9b3baa1b70f5e5ac8d67d3e6031d61d
Robin Walloner <wallonerrobin@gmail.com>
2026-08-03 15:59:55 +0200

    Configure port binding to use Scaleways PORT env variable

--- TodoDemo/Program.cs
+++ TodoDemo/Program.cs
@@ -4,6 +4,16 @@ using TodoDemo.Database;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Listen on configured port or default to 8080
+// Scaleway serverless containers set the PORT environment variable.
+// Once the port defined this way is bound, Scaleway considers the container healthy.
+// On platforms where PORT is not set (e.g. locally), we default to 8080
+int port = int.TryParse(builder.Configuration["PORT"], out var p) ? p : 8080;
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(port);
+});
+
 
 // ---- Services ----
 // ------------------
